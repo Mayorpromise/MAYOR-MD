81dc0fc54f69a981aa426d85adfa6e3ee9a5ce07
Excel Amadi <ms.excel.amadi@gmail.com>
2024-06-09 13:37:53 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -14,14 +14,14 @@ const Secktor = require('../lib')
 Secktor.cmd({
         pattern: "ping",
         desc: "To check ping",
-        category: "general",
+        category: "info",
         filename: __filename,
     },
     async(Void, citel) => {
         var inital = new Date().getTime();
-        const { key } = await Void.sendMessage(citel.chat, {text: '```Ping!!!```'});
+        const { key } = await Void.sendMessage(citel.chat, {text: '```Mayor_is_Pinging!!!```'});
         var final = new Date().getTime();
        // await Secktor.sleep(1000)
-       return await Void.sendMessage(citel.chat, {text: '*Pong*\n *' + (final - inital) + ' ms* ', edit: key});
+       return await Void.sendMessage(citel.chat, {text: '*🏓Pong*\n *' + (final - inital) + ' ms* ', edit: key});
     }
 );
